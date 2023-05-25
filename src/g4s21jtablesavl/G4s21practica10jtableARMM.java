6223e5cff43a0d34aac1f8fc351864588bf47084
Vazquez Lopez Sergio Aaron <>
2023-05-24 21:51:00 -0600

    Tarea Jtable

---
+++ src/g4s21jtablesavl/G4s21practica10jtableARMM.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21jtablesavl;
+
+/**
+ *
+ * @author shack
+ */
+public class G4s21practica10jtableARMM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new jforincipal().setVisible(true);
+    }
+    
+}
